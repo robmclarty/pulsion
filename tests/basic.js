ff094ba74ccaa1bb541e56d9b0a62723b46087b1
slode <rob.m@myplanet.com>
2017-02-21 16:09:48 -0500

    basic connection + authentication flow

--- tests/basic.js
+++ tests/basic.js
@@ -1,11 +1,19 @@
 import Pulsion from '../lib'
 
 const pulsion = Pulsion.init({
-  url: 'ws://localhost:3000/ws'
+  url: 'ws://localhost:3001/chat'
 })
 
 pulsion.on('open', e => {
-  console.log('socket opened')
+  console.log('socket opened', e)
 
   pulsion.send('this is my message')
+})
+
+pulsion.on('message', msg => {
+  console.log('received: ', msg)
+})
+
+pulsion.on('close', e => {
+  console.log('socket closed')
 })
